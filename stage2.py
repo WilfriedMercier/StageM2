1b86f73f92913dd386fd46b7074c5ca3b19adbc1
Wilfried Mercier <wilfried.mercier58@gmail.com>
2019-04-02 15:34:35 +0200

    python module with useful functions for the intership such as computing and applying masks for many arrays and plotting graphs with plenty of useful options in a single command

---
+++ stage2.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  2 09:32:44 2019
+
+@author: wilfried
+"""
+
+from sys import exit
+
+from astropy.io.votable import is_votable
+import numpy as np
+import matplotlib.pyplot as plt
+
+def is_VOtable(fullname):
+    """
+    Check whether a file is a VOtable.
+    
+    Input
+    ------
+    fullname : string
+        path+name of the file to test
+    
+    Returns True if it is a VOtable. False otherwise.
+    """
+    tag = is_votable(fullname)
+    print("The file", fullname, "is a VOtable, right ?", tag)
+    return tag
+
+def linear_fit(x, A, offset):
+    """
+    Compute a linear relation A*x+offset.
+    
+    Input
+    -----
+    x : numpy array
+        input data
+    A : float
+        Slope coefficient
+    offset : float
+        x=0 Y-coordinate
+        
+    Returns a numpy array A*x+offset.
+    """
+    return A*x+offset
+
+def maskToRemoveVal(listOfArrays, val=None, keep=True, astroTableMask=False):
+    """
+    Computes a mask by finding occurences in a list of arrays.
+    
+    Input
+    -----
+    listOfArrays : list of numpy arrays
+        the list of arrays from which the mask is built
+    val : float or None
+        the value to find. If val=None, it looks for nan values.
+    keep : boolean
+        if True, it builds a mask with True everywhere the value val is encountered. If False, it does the opposite
+    astroTableMask : boolean
+        if True returns a mask from the astropy table column instead of looking for some value/nans with False values everywhere the data is masked
+    
+    Returns a mask as a numpy array.
+    """
+    
+    shp = listOfArrays[0].shape
+    #Checking that arrays have the same shape
+    for array in listOfArrays[1:]:
+        if shp != array.shape:
+            exit("Arrays do not have the same dimensions, thus making the masking operation unfit. Exiting.")
+  
+    #Constructing first mask
+    if astroTableMask:
+        tmp = np.logical_not(listOfArrays[0].mask)
+    elif val is None:
+        tmp = np.logical_not(np.isnan(listOfArrays[0]))
+    else:
+        tmp = listOfArrays[0] == val
+        if not keep:
+            tmp = np.logical_not(tmp)
+        
+    #Applying logical and on all the masks
+    for (num, array) in enumerate(listOfArrays[1:]):
+        #consider we are looking for nan in the arrays
+        if astroTableMask:
+            tmp = np.logical_and(tmp, np.logcial_not(listOfArrays[0].mask))  
+        elif val is None:
+            tmp = np.logical_and(tmp, np.logical_not(np.isnan(array)))
+        else:
+            if keep:
+                tmp = np.logical_and(tmp, array==val)
+            else:
+                tmp = np.logical_and(tmp, array != val)
+    return tmp
+
+def applyMask(listOfArrays, mask):
+    """
+    Apply the same mask to a list of arrays and return the new arrays.
+    
+    Input
+    -----
+    listOfArrays : list of numpy arrays
+        the list of arrays the mask is applied to
+    mask : numpy array
+        the mask to apply
+        
+    Returns the list of arrays with the mask applied. If len(listOfArrays) is 1, it returns only an array instead of a list of arrays with one object.
+    """
+
+    for (num, array) in enumerate(listOfArrays):
+        if len(listOfArrays) == 1:
+            listOfArrays = array[mask]
+        else:
+            listOfArrays[num] = array[mask]
+    return listOfArrays
+
+def findWhereIsValue(listOfArrays, val=None):
+    """
+    Find and print the first position where a value is found within a list of arrays.
+    
+    Input
+    -----
+    listOfArrays : list of numpy arrays
+        list from which the value val is searched
+    val : float or None
+        value to look for. If val=None, it looks for nan values.
+    """
+    
+    for (num, array) in enumerate(listOfArrays):
+        if val is None:
+            if np.any(np.isnan(array)):
+                print("A nan was found at position", np.where(np.isnan(array))[0], "within array number", num)
+            else:
+                print("No nan was found in array number", num)
+        else:
+            if np.asarray(np.where(array==val)).shape[1] == 0:
+                print("No value", val, "found within array number", num)
+            else:
+                print("Value", val, "found at position", np.where((array==val))[0], "within array number", num)
+                
+                
+def asManyPlots(numPlot, datax, datay, hideXlabel=False, hideYlabel=False, hideYticks=False,
+                placeYaxisOnRight=False, xlabel="", ylabel='', marker='o', color='black', plotFlag=True,
+                label='', zorder=0, textsize=24, showLegend=False, legendTextSize=24, linestyle='None',
+                ylim=[None, None], xlim=[None, None], cmap=None, cmapMin=None, cmapMax=None,
+                showColorbar=False, locLegend='best', tickSize=24):
+    """
+    Function which plots on a configurable subplot grid either with pyplot.plot or pyplot.scatter.
+    
+    Input
+    -----
+    numPlot : int (3 digits)
+        the subplot number
+    datax: list, numpy array
+        the x data
+    datay :list, numpy array
+        the y data
+    hideXlabel : boolean
+        whether to hide the x label or not
+    hideYlabel : boolean
+        whether to hide the y label or not
+    hideYticks : boolean
+        whether to hide the y ticks or not
+    placeYaxisOnRight : boolean
+        whether to place the y axis of the plot on the right or not
+    xlabel : string
+        the x label
+    ylabel : string
+        the y label
+    marker : string, char (pyplot symbol)
+        the marker to use for the data
+    color : string/char/RGB/list of values
+        the color for the data. If a list of values is given, plotFlag must be False and a cmap must be given.
+    plotFlag : boolean
+        if True, plots with pyplot.plot function. If False, use pyplot.scatter
+    label : string
+        legend label for the data
+    zorder : int
+        whether the data will be plot in first position or in last. The lower the value, the earlier it will be plotted
+    textsize : int
+        size for the labels
+    showLegend : boolean
+        whether to show the legend or not
+    legendTextSize : int
+        size for the legend
+    linestyle : string
+        which line style to use
+    ylim : list of floats/None
+        the y-axis limits to use. If None is specified as lower/upper/both limit(s), the minimum/maximum/both values are used
+    xlim : list of floats/None
+        the x-axis limits to use. If None is specified as lower/upper/both limit(s), the minimum/maximum/both values are used
+    cmap : matplotlib colormap
+        the colormap to use for the scatter plot only
+    cmapMin: float
+        the minmum value for the colormap
+    cmapMax: float
+        the maximum value for the colormap
+    showColorbar : boolean
+        whether to show the colorbar for a scatter plot or not
+    locLegend : string, int
+        position where to place the legend
+    tickSize : int
+        size of the ticks on both axes
+        
+    Return current axis and last plot.
+    """
+    
+    ax1 = plt.subplot(numPlot)
+    ax1.yaxis.set_ticks_position('both')
+    ax1.xaxis.set_ticks_position('both')
+    ax1.tick_params(which='both', direction='in', labelsize=tickSize)
+    plt.grid()
+    
+    #hiding labels if required
+    if hideXlabel:
+        ax1.axes.get_xaxis().set_ticklabels([])
+    else:
+        plt.xlabel(xlabel, size=textsize)    
+    if hideYticks:
+        ax1.axes.get_yaxis().set_ticklabels([])
+    if not hideYlabel:    
+        plt.ylabel(ylabel, size=textsize)
+    
+    #Place Y axis on the right if required
+    if placeYaxisOnRight:
+        ax1.yaxis.tick_right()
+        ax1.yaxis.set_label_position("right")
+    
+    #Plotting
+    if plotFlag:
+        tmp = plt.plot(datax, datay, label=label, marker=marker, color=color, zorder=zorder, linestyle=linestyle)
+    else:
+        if cmapMin is None:
+            cmapMin = np.min(color)
+        if cmapMax is None:
+            cmapMax = np.max(color)
+        
+        tmp = plt.scatter(datax, datay, label=label, marker=marker, c=color, zorder=zorder, 
+                          cmap=cmap, vmin=cmapMin, vmax=cmapMax)
+        
+        if showColorbar:
+            plt.colorbar(tmp)
+            
+    if showLegend:
+        plt.legend(loc=locLegend, prop={'size': legendTextSize})
+        
+    #Define Y limits if required
+    if ylim[0] is not None:
+        ax1.set_ylim(bottom=ylim[0])
+#     else:
+#         ax1.set_ylim(bottom=ax.get_ylim()[0])
+    if ylim[1] is not None:
+        ax1.set_ylim(top=ylim[1])
+#     else:
+#         ax1.set_ylim(top=ax.get_ylim()[1])
+        
+    #define X limits if required
+    if xlim[0] is not None:
+        ax1.set_xlim(left=xlim[0])
+#     else:
+#         ax1.set_xlim(left=ax.get_xlim()[0])
+    if xlim[1] is not None:
+        ax1.set_xlim(right=xlim[1])
+#     else:
+#         ax1.set_xlim(right=ax.get_xlim()[1])
+    
+    return ax1, tmp
